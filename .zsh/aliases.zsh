07e1c9bdb2566d69fe41267a329ff4f6955c7177
aereal <aereal.h@gmail.com>
2010-09-20 02:40:33 +0900

    エイリアスを追加

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,10 +1,12 @@
 # vim:set ft=zsh:
 
 alias ..='cd ..'
-alias l='/bin/ls --color -AF'
-alias ls='/bin/ls --color -F'
+alias l='/bin/ls --color -F'
+alias ls='/bin/ls --color -AF'
 alias la='/bin/ls --color -AFl'
-alias unt='tar zxvf '
+alias unt='tar zxvf'
 alias s='screen'
 alias :q='exit'
+alias ps='ps aux'
+alias sudo='sudo env PATH=$PATH'
 
