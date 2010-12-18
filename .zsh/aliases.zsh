73bb31b2f861a1da9fb23ae1e000fce979a3818f
aereal <aereal.h@gmail.com>
2010-12-19 01:37:05 +0900

    sudoのPATHは自分でどうにかする

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,12 +1,11 @@
 # vim:set ft=zsh:
 
 alias ..='cd ..'
-alias l='/bin/ls --color -F'
-alias ls='/bin/ls --color -AF'
-alias la='/bin/ls --color -AFl'
+alias l='ls --color -F'
+alias ls='ls --color -AF'
+alias la='ls --color -AFl'
 alias unt='tar zxvf'
 alias s='screen'
 alias :q='exit'
 alias ps='ps aux'
-alias sudo='PATH=$PATH sudo'
 
