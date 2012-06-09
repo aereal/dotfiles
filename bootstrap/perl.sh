b20b06c9eb437c827a811331f6891238c6e7676d
aereal <aereal@kerare.org>
2012-06-09 15:17:52 +0900

    Switch to installed Perl after installation

--- bootstrap/perl.sh
+++ bootstrap/perl.sh
@@ -1,10 +1,15 @@
 #!/bin/sh
 
+__PERL_VERSION='perl-5.14.2'
 echo "Install perlbrew"
 curl -kL http://install.perlbrew.pl | bash
 
-echo "Install Perl 5.14.2"
-perlbrew install perl-5.14.2
+echo "Install $__PERL_VERSION"
+perlbrew install $__PERL_VERSION
 
 echo "Install cpanm"
 perlbrew install-cpanm
+
+
+echo "Switch to $__PERL_VERSION"
+perlbrew switch $__PERL_VERSION
