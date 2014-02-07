18c5dc321112046830851f8eefe23775898efe7d
aereal <aereal@aereal.org>
2014-02-07 11:36:24 +0900

    fish: Customize syntax highlight colors

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,20 @@
+# Configuration {{{
 set fish_greeting ""
 
+set fish_color_normal       normal
+set fish_color_command      blue
+set fish_color_quote        yellow
+set fish_color_end          black
+set fish_color_error        red
+set fish_color_param        normal -bold
+set fish_color_comment      black
+set fish_color_match        green
+set fish_color_search_match yellow
+set fish_color_operator     yellow
+set fish_color_escape       brown
+# }}}
+
+# Environment Variable {{{
 set -x PATH /opt/homebrew/bin $HOME/.rbenv/shims $HOME/.rbenv/bin $HOME/.plenv/shims $HOME/.plenv/bin /usr/bin /usr/sbin /bin /sbin
 set -x PATH $HOME/bin $PATH
 
@@ -7,8 +22,9 @@ set -U PAGER less
 set -U LESS "--LONG-PROMPT --RAW-CONTROL-CHARS"
 set -U MACVIM_APP /Applications/MacVim.app/Contents/MacOS/Vim
 set -U EDITOR "reatach-to-user-namespace -l $MACVIM_APP"
+# }}}
 
-function fish_prompt
+function fish_prompt # {{{
   set -l last_status $status
   printf ' '
   if test $last_status -eq 0
@@ -20,31 +36,37 @@ function fish_prompt
   end
   set_color normal
   printf " < \n"
-end
+end # }}}
 
+# Git prompt {{{
 set -g __fish_git_prompt_color_branch magenta
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_color_dirtystate blue
 set -g __fish_git_prompt_color_stagedstate yellow
 set -g __fish_git_prompt_color_invalidstate red
 set -g __fish_git_prompt_color_cleanstate green
-function fish_right_prompt
+# }}}
+
+function fish_right_prompt # {{{
   printf "%s " (__fish_git_prompt)
   set_color cyan
   prompt_pwd
-end
+end # }}}
 
-function mvim
+function mvim # {{{
   reattach-to-user-namespace -l $MACVIM_APP --remote-silent $argv
-end
+end # }}}
 
-function vim
+function vim # {{{
   env LANG=en_US.UTF-8 reattach-to-user-namespace -l $MACVIM_APP $argv
-end
+end # }}}
 
+# Alias {{{
 alias l ls\ -AFG
 alias ll ls\ -AFGl
+# }}}
 
+# Launch tmux {{{
 if which tmux >/dev/null; and test -z $TMUX
   if tmux has-session >/dev/null
     tmux attach-session
@@ -52,3 +74,6 @@ if which tmux >/dev/null; and test -z $TMUX
     tmux new-session
   end
 end
+# }}}
+
+# vim:set foldmethod=marker:
