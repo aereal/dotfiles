87788f77e45a99576971aa655859edef5517d8fe
aereal <aereal@aereal.org>
2015-02-14 23:14:10 +0900

    Configure Hatena Bookmark extension

--- .vimperatorrc.js
+++ .vimperatorrc.js
@@ -1,3 +1,9 @@
+liberator.globalVariables.hBookmark_shortcuts = {
+  hintsAdd: 'Ba',
+  hintsComment: 'Bc',
+  add: ['C'],
+  comment: ['c'],
+};
 if (typeof hBookmark != 'undefined') {
   liberator.loadScript('chrome://hatenabookmark/content/vimperator/plugin/hatenabookmark.js', {
     __proto__: this
