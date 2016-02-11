ff9567f53b841dd31a476596140a581d48ac5eca
aereal <aereal@aereal.org>
2016-02-12 00:23:45 +0900

    slate: Separate ops definitions and key-bindings

--- .slate.js
+++ .slate.js
@@ -10,53 +10,63 @@ var vi = function(key) {
   return [key, VI_LIKE_WINDOW_PLACEMENT_PREFIX_KEY, 'toggle'].join(':');
 };
 
+var pushLeftOp = slate.op('push', {
+  direction: 'left',
+  style: 'bar-resize:screenSizeX/2'
+});
+var pushDownOp = slate.op('push', {
+  direction: 'down',
+  style: 'bar-resize:screenSizeY/2'
+});
+var pushUpOp = slate.op('push', {
+  direction: 'up',
+  style: 'bar-resize:screenSizeY/2'
+});
+var pushRightOp = slate.op('push', {
+  direction: 'right',
+  style: 'bar-resize:screenSizeX/2'
+});
+var moveTopLeftOp = slate.op('move', {
+  x: 0,
+  y: 0,
+  width: 'screenSizeX/2',
+  height: 'screenSizeY/2'
+});
+var moveTopRightOp =  slate.op('move', {
+  x: 'screenSizeX/2',
+  y: 0,
+  width: 'screenSizeX/2',
+  height: 'screenSizeY/2'
+});
+var moveBottomRightOp =  S.op('move', {
+  x: 'screenSizeX/2',
+  y: 'screenSizeY/2',
+  width: 'screenSizeX/2',
+  height: 'screenSizeY/2'
+});
+var moveBottomLeftOp = slate.op('move', {
+  x: 0,
+  y: 'screenSizeY/2',
+  width: 'screenSizeX/2',
+  height: 'screenSizeY/2'
+});
+var moveFullOp = slate.op('move', {
+  x: 0,
+  y: 0,
+  width: 'screenSizeX',
+  height: 'screenSizeY'
+});
+
 var viLikeWindowPlacements = {
-  h: S.op('push', {
-    direction: 'left',
-    style: 'bar-resize:screenSizeX/2'
-  }),
-  j: S.op('push', {
-    direction: 'down',
-    style: 'bar-resize:screenSizeY/2'
-  }),
-  k: S.op('push', {
-    direction: 'up',
-    style: 'bar-resize:screenSizeY/2'
-  }),
-  l: S.op('push', {
-    direction: 'right',
-    style: 'bar-resize:screenSizeX/2'
-  }),
-  i: S.op('move', {
-    x: 0,
-    y: 0,
-    width: 'screenSizeX/2',
-    height: 'screenSizeY/2'
-  }),
-  o: S.op('move', {
-    x: 'screenSizeX/2',
-    y: 0,
-    width: 'screenSizeX/2',
-    height: 'screenSizeY/2'
-  }),
-  m: S.op('move', {
-    x: 'screenSizeX/2',
-    y: 'screenSizeY/2',
-    width: 'screenSizeX/2',
-    height: 'screenSizeY/2'
-  }),
-  n: S.op('move', {
-    x: 0,
-    y: 'screenSizeY/2',
-    width: 'screenSizeX/2',
-    height: 'screenSizeY/2'
-  }),
-  'return': S.op('move', {
-    x: 0,
-    y: 0,
-    width: 'screenSizeX',
-    height: 'screenSizeY'
-  })
+  h: pushLeftOp,
+  j: pushDownOp,
+  k: pushUpOp,
+  l: pushRightOp,
+  i: moveTopLeftOp,
+  o: moveTopRightOp,
+  m: moveBottomRightOp,
+  n: moveBottomLeftOp,
+  'return': moveFullOp,
 };
 
 for (var key in viLikeWindowPlacements) {
