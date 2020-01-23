d8c15406837030691d366fa9e74d0068267f75c1
aereal <aereal@aereal.org>
2020-01-23 18:44:33 +0900

    installer: create symlinks

--- cmd/install/main.go
+++ cmd/install/main.go
@@ -7,6 +7,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"path/filepath"
 )
 
 func main() {
@@ -26,7 +27,37 @@ func run(argv []string) error {
 	flgs.Parse(argv[1:])
 
 	dotfilesRepo := &githubRepo{Owner: "aereal", Name: "dotfiles"}
-	if err := assumeInstalledRepo(root, dotfilesRepo); err != nil {
+	repoFullPath, err := assumeInstalledRepo(root, dotfilesRepo)
+	if err != nil {
+		return err
+	}
+
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return err
+	}
+
+	toSkip := skipMapping{
+		"test":             true,
+		".dotfiles.ignore": true,
+		".config":          true,
+		"README.md":        true,
+		"cmd":              true,
+		"colors":           true,
+		"osx":              true,
+		".gitmodules":      true,
+		".gitignore":       true,
+		".git":             true,
+		".travis.yml":      true,
+		"Rakefile":         true,
+		"brew.bash":        true,
+	}
+	srcs, err := collectConfigFiles(repoFullPath, toSkip)
+	if err != nil {
+		return err
+	}
+	err = linkFiles(srcs, homeDir)
+	if err != nil {
 		return err
 	}
 
@@ -46,16 +77,17 @@ func (r *githubRepo) URLForClone() string {
 	return fmt.Sprintf("https://github.com/%s/%s", r.Owner, r.Name)
 }
 
-func assumeInstalledRepo(root string, repo *githubRepo) error {
-	repoFullPath := path.Join(root, "src", "github.com", repo.Owner, repo.Name)
-	_, err := os.Stat(repoFullPath)
+func assumeInstalledRepo(root string, repo *githubRepo) (installedPath string, err error) {
+	installedPath = path.Join(root, "src", "github.com", repo.Owner, repo.Name)
+	_, err = os.Stat(installedPath)
 	if os.IsNotExist(err) {
-		return installRepo(repo, repoFullPath)
+		err = installRepo(repo, installedPath)
+		return
 	}
 	if err != nil {
-		return err
+		return
 	}
-	return nil
+	return
 }
 
 func installRepo(repo *githubRepo, dest string) error {
@@ -65,4 +97,55 @@ func installRepo(repo *githubRepo, dest string) error {
 		return err
 	}
 	return nil
+}
+
+func collectConfigFiles(root string, skipper skipper) ([]string, error) {
+	srcs := []string{}
+	matches, err := filepath.Glob("*")
+	if err != nil {
+		return nil, err
+	}
+	for _, base := range matches {
+		if skipper.Skip(base) {
+			continue
+		}
+		srcs = append(srcs, filepath.Join(root, base))
+	}
+	return srcs, nil
+}
+
+func linkFiles(srcs []string, destDir string) error {
+	for _, src := range srcs {
+		dest := filepath.Join(destDir, filepath.Base(src))
+		err := linkFile(src, dest)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func linkFile(src string, dest string) error {
+	err := os.Symlink(src, dest)
+	if os.IsExist(err) {
+		return nil
+	}
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func isSymlink(mode os.FileMode) bool {
+	return mode&os.ModeSymlink != 0
+}
+
+type skipper interface {
+	Skip(path string) bool
+}
+
+type skipMapping map[string]bool
+
+func (m skipMapping) Skip(path string) bool {
+	return m[path]
 }
