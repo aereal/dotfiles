e89bcd20935e2ae51b475ead70b960606c4098b8
aereal <aereal@aereal.org>
2020-01-23 22:20:23 +0900

    installer: install formulae

--- cmd/install/main.go
+++ cmd/install/main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
+	"io"
 	"log"
 	"os"
 	"os/exec"
 	"path"
 	"path/filepath"
+	"strings"
+
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -58,6 +63,55 @@ func run(argv []string) error {
 	}
 	err = linkFiles(srcs, homeDir)
 	if err != nil {
+		return err
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	installer := &homebrewInstall{
+		brewCommand: "brew",
+		formulae: []*formula{
+			newFormula("awscli"),
+			newFormula("bat"),
+			newFormula("coreutils"),
+			newFormula("direnv"),
+			newFormula("docker-completion"),
+			newFormula("docker-compose-completion"),
+			newFormula("envchain"),
+			newFormula("exa"),
+			newFormula("git"),
+			newFormula("go"),
+			newFormula("hub"),
+			newFormula("jq"),
+			newFormula("mysql-client"),
+			newFormula("neovim"),
+			newFormula("node"),
+			newFormula("node@10"),
+			newFormula("node@12"),
+			newFormula("peco"),
+			newFormula("proctools"),
+			newFormula("pstree"),
+			newFormula("python"),
+			newFormula("readline"),
+			newFormula("reattach-to-user-namespace"),
+			newFormula("ripgrep"),
+			newFormula("ruby"),
+			newFormula("sshuttle"),
+			newFormula("terraform"),
+			newFormula("tig"),
+			newFormula("tmux"),
+			newFormula("tree"),
+			newFormula("yarn"),
+			newFormula("zsh"),
+			newFormula("zsh-completions"),
+			newFormula("zsh-history-substring-search"),
+			newFormula("zsh-syntax-highlighting"),
+		},
+		outStream:   os.Stdout,
+		errorStream: os.Stderr,
+	}
+	if err := installer.run(ctx); err != nil {
 		return err
 	}
 
@@ -148,4 +202,71 @@ type skipMapping map[string]bool
 
 func (m skipMapping) Skip(path string) bool {
 	return m[path]
+}
+
+type homebrewInstall struct {
+	brewCommand string
+	formulae    []*formula
+	outStream   io.Writer
+	errorStream io.Writer
+}
+
+func (i *homebrewInstall) run(ctx context.Context) error {
+	var eg *errgroup.Group
+	eg, ctx = errgroup.WithContext(ctx)
+
+	for _, cmd := range i.installCommand(ctx) {
+		eg.Go(func() error {
+			log.Printf("run %s", strings.Join(cmd.Args, " "))
+			cmd.Stdout = i.outStream
+			cmd.Stderr = i.errorStream
+			return cmd.Run()
+		})
+	}
+
+	return eg.Wait()
+}
+
+func (i *homebrewInstall) installCommand(ctx context.Context) []*exec.Cmd {
+	cmds := []*exec.Cmd{}
+	if len(i.formulae) == 0 {
+		return cmds
+	}
+
+	wholeInstallCmd := exec.CommandContext(ctx, i.brewCommand, "install")
+	for _, f := range i.formulae {
+		if f.hasOptions() {
+			cmd := f.command(ctx, i.brewCommand)
+			cmds = append(cmds, cmd)
+		} else {
+			wholeInstallCmd.Args = append(wholeInstallCmd.Args, f.name)
+		}
+	}
+	cmds = append(cmds, wholeInstallCmd)
+	return cmds
+}
+
+type formula struct {
+	name    string
+	options []string
+}
+
+func newFormula(name string, options ...string) *formula {
+	return &formula{
+		name:    name,
+		options: options,
+	}
+}
+
+func (f *formula) hasOptions() bool {
+	return len(f.options) > 0
+}
+
+func (f *formula) command(ctx context.Context, brew string) *exec.Cmd {
+	cmd := exec.CommandContext(ctx, brew, "install")
+	for _, opt := range f.options {
+		cmd.Args = append(cmd.Args, opt)
+	}
+	cmd.Args = append(cmd.Args, f.name)
+	return cmd
 }
